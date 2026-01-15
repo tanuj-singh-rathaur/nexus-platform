a3b751fe4646bb35805e901544b2780417267d2a
Tanuj Singh Rathaur <tanujsingh635@gmail.com>
2026-01-15 14:56:50 +0530

    initialized nexus-platform

---
+++ stats-collector/src/main/java/com/rathaur/nexus/statscollector/StatsCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.rathaur.nexus.statscollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatsCollectorApplication.class, args);
+	}
+
+}
---
+++ stats-collector/src/test/java/com/rathaur/nexus/statscollector/StatsCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rathaur.nexus.statscollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
