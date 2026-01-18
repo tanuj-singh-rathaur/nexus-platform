d0d70fee1ed599a086c4aa54917316d6f1c2fcf9
Tanuj Singh Rathaur <tanujsingh635@gmail.com>
2026-01-18 19:10:39 +0530

    docker: containarized each service and used docker compose to run all as one, Zipkin,Micrometer: added for tracing purpose

--- identity-service/src/main/java/com/rathaur/nexus/identityservice/controller/AuthController.java
+++ identity-service/src/main/java/com/rathaur/nexus/identityservice/controller/AuthController.java
@@ -3,14 +3,16 @@ package com.rathaur.nexus.identityservice.controller;
 import com.rathaur.nexus.identityservice.dto.AuthRequest;
 import com.rathaur.nexus.identityservice.entity.UserCredential;
 import com.rathaur.nexus.identityservice.service.AuthService;
+
+import jakarta.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Tanuj Singh Rathaur
@@ -23,19 +25,24 @@ public class AuthController {
 
     private AuthService authService;
     private AuthenticationManager authenticationManager;
+    
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
 
     public AuthController(AuthService authService, AuthenticationManager authenticationManager){
         this.authService =authService;
         this.authenticationManager = authenticationManager;
     }
 
+
     @PostMapping("/register")
-    public String registerUser(@RequestBody UserCredential userCredential){
+    public String registerUser(@RequestBody @Valid UserCredential userCredential){
+        log.info("📢 TRACE CHECK: Registering user {}", userCredential.getName());
         return authService.saveUser(userCredential);
     }
 
     @PostMapping("/token")
     public String getToken(@RequestBody AuthRequest authRequest){
+        log.info("📢 TRACE CHECK: Registering user {}", authRequest.getUsername());
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
         if(authentication.isAuthenticated())
             return authService.generateToken(authRequest.getUsername());
